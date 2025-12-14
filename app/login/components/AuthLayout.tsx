72f55f523d16e4aca9c0f6cdc3569220de6cb38d
Ridzwan Gigih <ridzwangigih3@gmail.com>
2025-12-15 05:31:18 +0700

    feat(login): adjust image on login page

--- app/login/components/AuthLayout.tsx
+++ app/login/components/AuthLayout.tsx
@@ -4,7 +4,7 @@ export function AuthLayout({ children }: { children: React.ReactNode }) {
         <div className="grid grid-cols-1 lg:grid-cols-2 border border-black/10 min-h-[600px]">
           {/* Left */}
           
-          <img src="/images/feature-images.png" alt="Login Background" className="w-full h-full object-cover" />
+          <img src="/images/login-image.jpg" alt="Login Background" className="w-full h-full object-cover" />
   
           {/* Right */}
           <div className="p-12 flex items-center justify-center">
