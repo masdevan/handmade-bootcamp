0dcd3c5204a531b4ec0fcbdc13d334d3d0d5e4d1
Ridzwan Gigih <ridzwangigih3@gmail.com>
2025-12-14 21:20:43 +0700

    chore(layout): add sidebar & header on layout admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,16 +1,16 @@
 import "@/styles/satoshi.css";
 import "@/styles/style.css";
 
-// import { Sidebar } from "@/components/Layouts/sidebar";
+import { Sidebar } from "@/components/admin/Layouts/sidebar";
 
 import "flatpickr/dist/flatpickr.min.css";
 import "jsvectormap/dist/jsvectormap.css";
 
-// import { Header } from "@/components/Layouts/header";
+import { Header } from "@/components/admin/Layouts/header";
 import type { Metadata } from "next";
-// import NextTopLoader from "nextjs-toploader";
+import NextTopLoader from "nextjs-toploader";
 import type { PropsWithChildren } from "react";
-// import { Providers } from "./providers";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: {
@@ -23,25 +23,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <h1>hai sayangggggg</h1>
-    // <html lang="en" suppressHydrationWarning>
-    //   <body>
-    //     <Providers>
-    //       <NextTopLoader color="#5750F1" showSpinner={false} />
+      <Providers>
+        <NextTopLoader color="#5750F1" showSpinner={false} />
 
-    //       <div className="flex min-h-screen">
-    //         <Sidebar />
+        <div className="flex min-h-screen">
+          <Sidebar />
 
-    //         <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
-    //           <Header />
+          <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
+            <Header />
 
-    //           <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
-    //             {children}
-    //           </main>
-    //         </div>
-    //       </div>
-    //     </Providers>
-    //   </body>
-    // </html>
+            <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
+              {children}
+            </main>
+          </div>
+        </div>
+      </Providers>
   );
 }
