2603e8ad613379f9158ce282753dd526aa81fe25
Ridzwan Gigih <ridzwangigih3@gmail.com>
2025-12-24 01:16:21 +0700

    feat(footer): delete support section

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
   return (
     <footer className="bg-white text-black border-t border-gray-200 shadow-sm">
       <div className="max-w-7xl mx-auto py-20">
-        <div className="grid md:grid-cols-4 gap-12 mb-12">
+        <div className="grid md:grid-cols-3 gap-12 mb-12">
           {/* Brand */}
           <div className="flex flex-col gap-5">
             <Link href="/" className={`${meowScript.className} text-4xl text-[#C5A48E] flex flex-row items-center gap-2`}>
@@ -69,33 +69,6 @@ export function Footer() {
               <li>
                 <a href="/terms" className="text-[#857164] transition-colors">
                   Terms
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* Support */}
-          <div>
-            <h4 className="font-bold mb-4 text-[#ba9a85]">Support</h4>
-            <ul className="space-y-2 text-gray-400">
-              <li>
-                <a href="/help" className="text-[#857164] transition-colors">
-                  Help Center
-                </a>
-              </li>
-              <li>
-                <a href="/shipping" className="text-[#857164] transition-colors">
-                  Shipping Info
-                </a>
-              </li>
-              <li>
-                <a href="/returns" className="text-[#857164] transition-colors">
-                  Returns
-                </a>
-              </li>
-              <li>
-                <a href="/faq" className="text-[#857164] transition-colors">
-                  FAQ
                 </a>
               </li>
             </ul>
