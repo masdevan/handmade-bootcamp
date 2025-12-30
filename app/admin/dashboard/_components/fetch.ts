a472830ce7424c5b30c1b964cbc47b33b2ff74c2
Ridzwan Gigih <ridzwangigih3@gmail.com>
2025-12-30 15:11:18 +0700

    feat(dashboard): Adjust Card Overview Dashboard Data

--- app/admin/dashboard/_components/fetch.ts
+++ app/admin/dashboard/_components/fetch.ts
@@ -1,91 +1,37 @@
+import { prisma } from "@/lib/prisma";
+
 export async function getOverviewData() {
-  // Fake delay
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const [
+    totalOrders,
+    totalProfit,
+    totalProducts,
+    totalUsers,
+  ] = await Promise.all([
+    prisma.order.count(),
+
+    prisma.order.aggregate({
+      _sum: {
+        totalPrice: true,
+      },
+    }),
+
+    prisma.product.count(),
+
+    prisma.user.count(),
+  ]);
 
   return {
     views: {
-      value: 3456,
-      growthRate: 0.43,
+      value: totalOrders,
     },
     profit: {
-      value: 4220,
-      growthRate: 4.35,
+      value: totalProfit._sum.totalPrice ?? 0,
     },
     products: {
-      value: 3456,
-      growthRate: 2.59,
+      value: totalProducts,
     },
     users: {
-      value: 3456,
-      growthRate: -0.95,
+      value: totalUsers,
     },
   };
-}
-
-export async function getChatsData() {
-  // Fake delay
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return [
-    {
-      name: "Jacob Jones",
-      profile: "/images/user/user-01.png",
-      isActive: true,
-      lastMessage: {
-        content: "See you tomorrow at the meeting!",
-        type: "text",
-        timestamp: "2024-12-19T14:30:00Z",
-        isRead: false,
-      },
-      unreadCount: 3,
-    },
-    {
-      name: "Wilium Smith",
-      profile: "/images/user/user-03.png",
-      isActive: true,
-      lastMessage: {
-        content: "Thanks for the update",
-        type: "text",
-        timestamp: "2024-12-19T10:15:00Z",
-        isRead: true,
-      },
-      unreadCount: 0,
-    },
-    {
-      name: "Johurul Haque",
-      profile: "/images/user/user-04.png",
-      isActive: false,
-      lastMessage: {
-        content: "What's up?",
-        type: "text",
-        timestamp: "2024-12-19T10:15:00Z",
-        isRead: true,
-      },
-      unreadCount: 0,
-    },
-    {
-      name: "M. Chowdhury",
-      profile: "/images/user/user-05.png",
-      isActive: false,
-      lastMessage: {
-        content: "Where are you now?",
-        type: "text",
-        timestamp: "2024-12-19T10:15:00Z",
-        isRead: true,
-      },
-      unreadCount: 2,
-    },
-    {
-      name: "Akagami",
-      profile: "/images/user/user-07.png",
-      isActive: false,
-      lastMessage: {
-        content: "Hey, how are you?",
-        type: "text",
-        timestamp: "2024-12-19T10:15:00Z",
-        isRead: true,
-      },
-      unreadCount: 0,
-    },
-  ];
 }
--- app/admin/dashboard/_components/overview-cards/index.tsx
+++ app/admin/dashboard/_components/overview-cards/index.tsx
@@ -9,7 +9,7 @@ export async function OverviewCardsGroup() {
   return (
     <div className="grid gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-4 2xl:gap-7.5">
       <OverviewCard
-        label="Total Views"
+        label="Total Orders"
         data={{
           ...views,
           value: compactFormat(views.value),
@@ -18,7 +18,7 @@ export async function OverviewCardsGroup() {
       />
 
       <OverviewCard
-        label="Total Profit"
+        label="Total Income"
         data={{
           ...profit,
           value: "$" + compactFormat(profit.value),
